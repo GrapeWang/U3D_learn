803cb3cc2c51a36bd5a0c69006ff279e74b262e1
GrapeWang <47807868+GrapeWang@users.noreply.github.com>
2019-03-04 22:20:55 +0800

    stick

---
+++ StickPin/Assets/Scripts/GameManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    private Transform startpoint;
+    private Transform initialpoint;
+    public GameObject pin;
+    private Pin currentPin;
+    private bool isGameover = false;
+    private float score = 0;
+    public Text scoreText;
+    private Camera maincamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maincamera = Camera.main;
+        startpoint = GameObject.Find("StartPoint").transform;
+        initialpoint = GameObject.Find("InitialPoint").transform;
+        initialPin();
+    }
+
+    private void Update()
+    {
+        if (isGameover) return;
+        if (Input.GetMouseButtonDown(0))
+        {
+            score++;
+            scoreText.text = score.ToString();
+            currentPin.FlyPin();
+            initialPin();
+        }
+    }
+
+    void initialPin()
+    {
+        currentPin = GameObject.Instantiate(pin, initialpoint.position, pin.transform.rotation).GetComponent<Pin>();
+    }
+
+    public void GameOver()
+    {
+        if(isGameover) return;
+        isGameover = true;
+        GameObject.Find("Circle").GetComponent<Rotate>().enabled = false;
+        StartCoroutine(GameoverAnim());
+    }
+
+    IEnumerator GameoverAnim()
+    {
+        while (true)
+        {
+            maincamera.backgroundColor = Color.Lerp(maincamera.backgroundColor, Color.red, 3 * Time.deltaTime);
+            maincamera.orthographicSize = Mathf.Lerp(maincamera.orthographicSize, 4, 3 * Time.deltaTime);
+            yield return 0;
+            if (Mathf.Abs(maincamera.orthographicSize - 4) <= 0.01f)
+            {
+                break;
+            }
+        }
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+}
---
+++ StickPin/Assets/Scripts/Pin.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pin : MonoBehaviour
+{
+    private bool isFly = false;
+    private bool isReady = false;
+    private Transform startpoint;
+    private Transform circlepoint;
+    private Vector3 tarPos;
+    public float speed = 15;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        startpoint = GameObject.Find("StartPoint").transform;
+        circlepoint = GameObject.Find("Circle").transform;
+        tarPos = circlepoint.position;
+        tarPos.y -= 1.55f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFly == false)
+        {
+            if (isReady == false)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, startpoint.position, 5f * Time.deltaTime);
+                if (Vector3.Distance(transform.position, startpoint.position) <= 0.05f)
+                {
+                    isReady = true;
+                }
+            }
+        }
+        else
+        {
+            transform.position = Vector3.MoveTowards(transform.position, tarPos, speed * Time.deltaTime);
+            if (Vector3.Distance(transform.position, tarPos) <= 0.05f)
+            {
+                transform.position = tarPos;
+                transform.parent = circlepoint;
+                isFly = false;
+            }
+        }
+    }
+
+    public void FlyPin()
+    {
+        isFly = true;
+        isReady = true;
+    }
+}
